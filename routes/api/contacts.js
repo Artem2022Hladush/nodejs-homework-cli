c5d9e95586bdb43239a58bda7e4548506f01dc7e
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-22 09:41:20 +0200

    add routes getAll

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,18 @@
 const express = require('express')
 
+const contactsOperations = require("../../models/contacts");
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactsList = await contactsOperations.listContacts();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contactsList,
+    },
+  })
 })
 
 router.get('/:contactId', async (req, res, next) => {
