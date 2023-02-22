ad8ae1eb22adf063226c82023d0dd131768992f4
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-22 11:10:11 +0200

    add helpers/HttpError, add getById

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
 const contactsOperations = require("../../models/contacts");
+const {HttpError} =  require("../../helpers")
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  const contactsList = await contactsOperations.listContacts();
+  try {
+    const contactsList = await contactsOperations.listContacts();
   res.json({
     status: "success",
     code: 200,
@@ -13,10 +15,31 @@ router.get('/', async (req, res, next) => {
       contactsList,
     },
   })
+  } catch (error) {
+  next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+try {
+  const {contactId} = req.params;
+  const contact = await contactsOperations.getContactById(contactId)
+
+if(!contact) {
+  throw HttpError(404, "Not found");
+}
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      contact,
+    },
+  })
+} catch (error) {
+  next(error)
+
+}
 })
 
 router.post('/', async (req, res, next) => {
