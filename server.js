ae2bceb4bce8075b65bf598f1291f34b0c545d60
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-27 22:48:52 +0200

    refact models

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require('./app');
 const mongoose = require("mongoose");
 const {DB_HOST} = process.env;
+const {PORT = 3000} = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful")
   })
   .catch(error => {
     console.log(`Server not running. Error message: ${error.message}`);
     process.exit(1)
   })
-// TaiAbPxkGXZiGdf7 mongoDB pass
