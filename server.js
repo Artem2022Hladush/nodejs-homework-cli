84069ee32f08a2e9bf6177981adc2132031d20cc
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-27 17:46:38 +0200

    init project mongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://Artem:TaiAbPxkGXZiGdf7@cluster0.getzcrz.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful")
+  })
+  .catch(error => {
+    console.log(`Server not running. Error message: ${error.message}`);
+    process.exit(1)
+  })
+// TaiAbPxkGXZiGdf7 mongoDB pass
