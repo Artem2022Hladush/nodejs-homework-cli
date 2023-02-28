c0515ee008b0a6a8fe47133af509098256fb511d
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-28 19:39:04 +0200

    add handleMongooseError

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require("mongoose");
+const {handleMongooseError} = require("../helpers")
 
 const contactSchema = new Schema({
   name: {
@@ -18,6 +19,8 @@ const contactSchema = new Schema({
 }, 
     {versionKey: false, timestamps: true,}
 );
+
+contactSchema.post("save", handleMongooseError)
 
 const Contact = model("contact", contactSchema)
 
