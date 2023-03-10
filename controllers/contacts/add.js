598794aec6a5250792b872888b5794e861e4e269
Артем Гладуш <m1hommage5008@gmail.com>
2023-03-10 16:52:09 +0200

    add: current, logout, authenti; RF add, getAll,

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -2,8 +2,9 @@ const {Contact} = require("../../models");
 
 
 const add = async (req, res) => {
+	const {_id: owner } =req.user;
 	const {body} = req;
-		const newContact = await Contact.create(body);
+		const newContact = await Contact.create({...body, owner});
 		res.json({
 			status: "success",
 			code: 201,
